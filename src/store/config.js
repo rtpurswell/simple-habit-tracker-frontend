9e1c6e3383af289538bc41ec0dfeafd4c2c464fb
Robert Purswell <taylor@tpurswell.com>
2023-02-04 09:03:59 -0700

    Revert "updated configs for new hosting"
    This reverts commit 97cb8f5b356e0de3b3518ea49b567b6b8cf88d4b.

--- src/store/config.js
+++ src/store/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiEndPoint: 'https://habit-nodejs.azurewebsites.net/',
+  apiEndPoint: 'https://rtpurswell-habit-tracker.herokuapp.com/',
   auth0Domain: 'dev-5e2zuayl.us.auth0.com',
   auth0ClientId: 'q9f0ETkP46pYmjDSPB2EAVW8w3y6bhzU',
 }
